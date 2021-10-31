0080c208cc4a217e00e6333ffc59fdce51690040
dpeilitz <dpeilitz@kth.se>
2021-10-31 22:24:41 +0100

    rough attempts at A different problem and cyber clara rough

---
+++ Cyber_Clara.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Compare integers.
+int cmp_int(const void *a, const void *b)
+{
+   return *(int *)a - *(int *)b;
+}
+
+int main()
+{
+   long guests;
+   long firstname[guests], lastname[guests], ;
+   ;
+   scanf("%d", &guests);
+   for (i = 0; i < guests; i++)
+      scanf("%s", &firstname[i];
+
+   for (i = 0; i < guests; i++)
+      scanf("%s", &lastname[i]);
+
+      if (&lastname[i])
+   // int i, size;
+   // int total = 0;
+   // int *numbers;
+   // scanf("%d", &size);
+   // numbers = malloc(size * sizeof(int));
+   // for (i = 0; i < size; i++)
+   //    scanf("%d", &numbers[i]);
+
+   // qsort((void *)numbers, (size_t)size, sizeof(int), cmp_int);
+
+   // for (i = 0; i < (size + 1) / 2; i++)
+   //    total += numbers[size - i - 1];
+   // printf("%d", total);
+}
